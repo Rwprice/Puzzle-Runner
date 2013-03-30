b9a89f9214d73f4c203fbd4fcc4c6070ea291f96
Ryan Price <Rwprice01@gmail.com>
2013-03-30 19:11:12 -0400

    Minor edit in Enemy class

--- Puzzle-Runner/src/com/earthquakeunicorn/puzzlerunner/animateobjects/Enemy.java
+++ Puzzle-Runner/src/com/earthquakeunicorn/puzzlerunner/animateobjects/Enemy.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.earthquakeunicorn.puzzlerunner.screens.GameScreen;
 
 public class Enemy extends AnimateObject 
 {
