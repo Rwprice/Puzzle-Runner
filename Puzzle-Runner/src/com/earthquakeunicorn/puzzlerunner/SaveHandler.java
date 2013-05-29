d885b14aad06a5b2152cd5d6f50fb85b3f1ba746
Ryan Price <Rwprice01@gmail.com>
2013-05-29 11:23:02 -0400

    Fixed iss

--- Puzzle-Runner/src/com/earthquakeunicorn/puzzlerunner/SaveHandler.java
+++ Puzzle-Runner/src/com/earthquakeunicorn/puzzlerunner/SaveHandler.java
@@ -2,15 +2,15 @@ package com.earthquakeunicorn.puzzlerunner;
 
 public class SaveHandler 
 {
-	public static void saveGame()
-	{
-		
-	}
-	
-	public static String loadGame()
-	{
-		
-		
-		return null;
-	}
+ public static void saveGame()
+ {
+  System.out.println("Not Just Nothing");
+ }
+ 
+ public static String loadGame()
+ {
+  
+  
+  return null;
+ }
 }
